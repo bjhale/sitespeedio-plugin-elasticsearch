42526edd8fe18209f8425bddacbd1535bd8e7108
billy.hale <billy.hale@gce.com>
2023-02-05 01:19:40 -0700

    Implement Data Collector, detect and include data from lighthouse plugin

--- dataCollector.js
+++ dataCollector.js
@@ -1,10 +1,94 @@
-'use strict'
+'use strict';
+
+const util = require('util');
+
 module.exports = class DataCollector {
   constructor(props) {
-    this.data = [];
+    this.urls = {};
   }
 
-  processPageSummary(message) {
+  getUrls() {
+    return Object.keys(this.urls);
+  }
 
+  getUrlData(url) {
+    return this.urls[url];
+  }
+
+  getData() {
+    return this.urls;
+  }
+
+  _addUrl(url) {
+    if (!this.urls[url]) {
+      this.urls[url] = {};
+    }
+  }
+
+  processLighthousePageSummary(message) {
+    this._addUrl(message.url);
+
+    let lighthouse = {};
+
+    lighthouse.categories = {};
+    Object.entries(message.data.categories).map(([key,value]) => {
+      lighthouse.categories[key] = value.score;
+    });
+
+    lighthouse.audits = {};
+    Object.entries(message.data.audits).map(([key,value]) => {
+      lighthouse.audits[key] = {};
+      lighthouse.audits[key].score = value.score;
+      if (value.numericValue) {
+        lighthouse.audits[key].value = value.numericValue;
+      }
+    });
+
+    this.urls[message.url]['lighthouse'] = lighthouse;
+  }
+
+  processBrowsertimePageSummary(message) {
+    this._addUrl(message.url);
+
+    const statistics = this._condenseStatisticsRecursive(
+      message.data.statistics
+    );
+
+    this.urls[message.url]['@timestamp'] = message.timestamp;
+    this.urls[message.url]['url'] = message.url;
+    this.urls[message.url]['domain'] = message.group;
+    this.urls[message.url]['connectivity'] = message.data.info.connectivity.profile;
+    this.urls[message.url]['browser'] = message.data.info.browser.name;
+    this.urls[message.url]['statistics'] = statistics;
+
+    // console.log(util.inspect(data, { depth: null }));
+    // process.exit();
+  }
+
+  _condenseStatisticsRecursive(data) {
+    // Escape when data.mean exists.
+    if (data.mean || data.mean === 0) {
+      return data.mean;
+    }
+
+    // Escape when encountering an array as arrays are typically not useful.
+    if (Array.isArray(data)) {
+      return null;
+    }
+
+    let d = {};
+
+    Object.entries(data).map(([key, value]) => {
+      const recursiveResult = this._condenseStatisticsRecursive(value);
+
+      // Eliminate null value keys from returned data.
+      if (recursiveResult === null) {
+        return;
+      }
+
+      d[key] = recursiveResult;
+    });
+
+    return d;
   }
 };
--- index.js
+++ index.js
@@ -67,41 +67,51 @@ module.exports = {
 
   async processMessage(message, queue) {
     switch (message.type) {
-      case 'browsertime.run':
-      case 'coach.run':
-        break;
       case 'browsertime.pageSummary':
-        await this.client.index({
-          index: this.options.index,
-          document: {
-            '@timestamp': message.timestamp,
-            url: message.data.info.browsertime.url,
-            browser: message.data.info.browser.name,
-            connectivity: message.data.info.connectivity.profile,
-            statistics: {
-              googleWebVitals: {
-                cumulativeLayoutShift: message.data.statistics.googleWebVitals.cumulativeLayoutShift.mean,
-                ttfb: message.data.statistics.googleWebVitals.ttfb.mean,
-                largestContentfulPaint: message.data.statistics.googleWebVitals.largestContentfulPaint.mean,
-                firstContentfulPaint: message.data.statistics.googleWebVitals.firstContentfulPaint.mean,
-                firstInputDelay: message.data.statistics.googleWebVitals.firstInputDelay.mean,
-                totalBlockingTime: message.data.statistics.googleWebVitals.totalBlockingTime.mean
-              },
-              coach: {
-                bestpractice: message.data.statistics.coach.coachAdvice.advice.bestpractice.score.mean,
-                performance: message.data.statistics.coach.coachAdvice.advice.performance.score.mean,
-                privacy: message.data.statistics.coach.coachAdvice.advice.privacy.score.mean,
-                total: message.data.statistics.coach.coachAdvice.advice.score.mean
-              }
-            }
-          }
-        });
+        this.dataCollector.processBrowsertimePageSummary(message);
+        // await this.client.index({
+        //   index: this.options.index,
+        //   document: {
+        //     '@timestamp': message.timestamp,
+        //     url: message.data.info.browsertime.url,
+        //     browser: message.data.info.browser.name,
+        //     connectivity: message.data.info.connectivity.profile,
+        //     statistics: {
+        //       googleWebVitals: {
+        //         cumulativeLayoutShift: message.data.statistics.googleWebVitals.cumulativeLayoutShift.mean,
+        //         ttfb: message.data.statistics.googleWebVitals.ttfb.mean,
+        //         largestContentfulPaint: message.data.statistics.googleWebVitals.largestContentfulPaint.mean,
+        //         firstContentfulPaint: message.data.statistics.googleWebVitals.firstContentfulPaint.mean,
+        //         firstInputDelay: message.data.statistics.googleWebVitals.firstInputDelay.mean,
+        //         totalBlockingTime: message.data.statistics.googleWebVitals.totalBlockingTime.mean
+        //       },
+        //       coach: {
+        //         bestpractice: message.data.statistics.coach.coachAdvice.advice.bestpractice.score.mean,
+        //         performance: message.data.statistics.coach.coachAdvice.advice.performance.score.mean,
+        //         privacy: message.data.statistics.coach.coachAdvice.advice.privacy.score.mean,
+        //         total: message.data.statistics.coach.coachAdvice.advice.score.mean
+        //       }
+        //     }
+        //   }
+        // });
+        break;
+      case 'lighthouse.pageSummary':
+        this.dataCollector.processLighthousePageSummary(message);
         break;
       case 'sitespeedio.render':
-        // this.storageManager.writeData(
-        //   JSON.stringify(this.messages, null, 2),
-        //   'elasticsearch-messages.json'
-        // );
+        let urls = this.dataCollector.getUrls();
+
+        for (const url of urls) {
+          await this.client.index({
+            index: this.options.index,
+            document: this.dataCollector.getUrlData(url)
+          });
+        }
+
+        await this.storageManager.writeData(
+          JSON.stringify(this.dataCollector.getData(), null, 2),
+          'elasticsearch-debug.json'
+        );
         break;
     }
   },
